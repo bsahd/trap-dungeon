0a12e879f0db11e14340a48c82ff223c61ade7af
bsahd <193844131+bsahd@users.noreply.github.com>
2025-09-04 17:41:26 +0900

    preact版: キーボードありなら全部プレイ可

--- preact_io.tsx
+++ preact_io.tsx
@@ -2,17 +2,76 @@
 import { Game } from "./game.ts";
 import { Fragment, h } from "preact";
 import { useEffect, useState } from "preact/hooks";
-import { DisplayState, GameI, Item } from "./interfaces.ts";
+import { DisplayState, GameI, GameLoopResult, Item } from "./interfaces.ts";
 import { ITEMS } from "./items.ts";
 import { UI_TEXT } from "./ui_text.ts";
 
-const LANG = "ja";
+export type Language = "ja" | "en";
+
+export function NotifyArea(props: { notifications: string[] }) {
+  return (
+    <>
+      {props.notifications.map((notify) => (
+        <div class="game-popup">{notify}</div>
+      ))}
+    </>
+  );
+}
+
+export function GameStatus(
+  props: { latestGameResult?: GameLoopResult; LANG: Language },
+) {
+  const LANG = props.LANG;
+  if (!props.latestGameResult) {
+    return <>Error: No DisplayState for GameStatus</>;
+  }
+  const d = props.latestGameResult;
+  const itemCounts = (d.displayState.items || []).reduce((counts, id) => {
+    counts[id] = (counts[id] || 0) + 1;
+    return counts;
+  }, {} as ({ [x: string]: number }));
+  return (
+    <div id="game-status">
+      <p id="floor-number">Floor: {d.displayState.floorNumber}</p>
+      Items:
+      <ul id="item-list">
+        {Object.entries(itemCounts).map((item) => {
+          return (
+            <li key={item[0]}>
+              <button
+                class="item-link"
+                onClick={() => alert(ITEMS[item[0]].description[LANG])}
+              >
+                {ITEMS[item[0]].name[LANG]}({ITEMS[item[0]].key}) x{item[1]}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {d.gameState == "gameover" && (
+        <>
+          {UI_TEXT.floorDisclosureRate[LANG]}
+          <ul>
+            {d.result.floorRevelationRates.length == 0 && (
+              <li>{UI_TEXT.none[LANG]}</li>
+            )}
+            {d.result.floorRevelationRates.map((revRate) => (
+              <li key={revRate.floor}>
+                {revRate.floor}: {(revRate.rate * 100).toFixed(0)}%
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
 
 export function GameGrid(
   props: { displayState?: DisplayState; runGameLoop: () => void },
 ) {
   if (!props.displayState) {
-    return <></>;
+    return <>Error: No DisplayState for GameGrid</>;
   }
   const displayState = props.displayState;
   return (
@@ -26,7 +85,8 @@ export function GameGrid(
               c === displayState.player.c;
             const isExit = r === displayState.exit.r &&
               c === displayState.exit.c;
-            const isRevealed = gridCell.isRevealed ||
+            const isRevealed = displayState.gameState == "gameover" ||
+              gridCell.isRevealed ||
               (isExit && displayState.exitRevealedThisFloor);
 
             // 開示されていて、かつ見通しの悪いマスの場合にのみスタイルを適用
@@ -91,7 +151,7 @@ export function GameGrid(
               >
                 <span
                   class={"cell-number" +
-                    (isPlayer ? "cell-number--player-present" : "")}
+                    (isPlayer ? " cell-number--player-present" : "")}
                 >
                   {numberContent}
                 </span>
@@ -115,6 +175,16 @@ gameInstance.setupFloor();
 
 export function GameMain() {
   const [displayState, setDisplayState] = useState<DisplayState>();
+  const [latestGameResult, setLatestGameResult] = useState<GameLoopResult>();
+  const [notifications, setNotifications] = useState<string[]>([]);
+  const [language, setLanguage] = useState<Language>("ja");
+  const notify = (text: string) => {
+    setNotifications((notifications) => notifications.concat(text));
+    setTimeout(
+      () => setNotifications((notifications) => notifications.slice(1)),
+      2000,
+    );
+  };
   const runGameLoop = (key?: string) => {
     if (key) {
       const actionResult = gameInstance.handleInput(key);
@@ -127,18 +197,19 @@ export function GameMain() {
       }
     }
     const gameResult = gameInstance.gameLoop();
-    console.log(gameResult);
 
     document.body.dataset.gameState = gameResult.gameState;
 
     if (gameResult.gameState != "gameover" && gameResult.newItemAcquired) {
       const item = gameResult.newItemAcquired;
-      const message = `${UI_TEXT.itemAcquisition[LANG]}: ${item.name[LANG]}`;
+      const message = `${UI_TEXT.itemAcquisition[language]}: ${
+        item.name[language]
+      }`;
       // showNotification(message, 3000);
-      alert(message);
+      notify(message);
       gameInstance.clearJustAcquiredItem();
     }
-
+    setLatestGameResult(gameResult);
     setDisplayState(gameResult.displayState);
 
     const gameState = gameResult.gameState;
@@ -147,11 +218,30 @@ export function GameMain() {
       // renderConfirmDialog(gameResult.message);
       runGameLoop(confirm(gameResult.message) ? "yes" : "no");
     } else if (gameState === "choosing_item") {
-      const itemNum = parseInt(
-        prompt(gameResult.displayState.currentItemChoices.join("\n")) ?? "",
-      );
+      let chosen: number | null = null;
+      const choices = gameResult.displayState.currentItemChoices;
+
+      while (chosen === null) {
+        const input = prompt(
+          UI_TEXT.chooseReward[language] + "\n" +
+            choices.map((id) => ITEMS[id].name[language]).join("\n"),
+        );
+        if (input === null) {
+          // キャンセル押されたら抜ける（ゲーム仕様次第）
+          break;
+        }
+        const num = parseInt(input);
+        console.log(input, num);
+        if (!isNaN(num) && num > 0 && num <= choices.length) {
+          chosen = num;
+          runGameLoop(num.toString());
+          break;
+        } else {
+          alert("Invalid choice, try again.");
+        }
+      }
     } else if (gameState === "gameover") {
-      alert(JSON.stringify(gameResult.result));
+      // notify(JSON.stringify(gameResult.result));
     } else if (
       ["playing", "jumping_direction", "recon_direction"].includes(gameState)
     ) {
@@ -159,7 +249,7 @@ export function GameMain() {
     }
 
     if (gameResult.gameState != "gameover" && gameResult.lastActionMessage) {
-      alert(gameResult.lastActionMessage[LANG]);
+      notify(gameResult.lastActionMessage[language]);
       gameInstance.clearLastActionMessage();
     }
 
@@ -179,7 +269,7 @@ export function GameMain() {
         "recon_direction",
       ].includes(gameState)
     ) {
-      alert(gameResult.message);
+      notify(gameResult.message);
     }
 
     if (
@@ -190,7 +280,7 @@ export function GameMain() {
     }
 
     if (gameResult.gameState != "gameover" && gameResult.tutorialToShow) {
-      alert(
+      notify(
         gameResult.tutorialToShow.title + "\n" +
           gameResult.tutorialToShow.content,
       );
@@ -315,40 +405,74 @@ export function GameMain() {
   }, []);
   return (
     <div>
-      <GameGrid displayState={displayState} runGameLoop={runGameLoop}>
-      </GameGrid>
-      <button
-        type="button"
-        onClick={() => {
-          runGameLoop("w");
-        }}
-      >
-        w
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          runGameLoop("a");
-        }}
-      >
-        a
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          runGameLoop("s");
-        }}
-      >
-        s
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          runGameLoop("d");
-        }}
-      >
-        d
-      </button>
+      <form action="#">
+        <label>
+          <input
+            type="radio"
+            name="language"
+            checked={language == "ja"}
+            onChange={(e) => {
+              e.currentTarget.checked && setLanguage("ja");
+            }}
+          />日本語
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="language"
+            checked={language == "en"}
+            onChange={(e) => {
+              e.currentTarget.checked && setLanguage("en");
+            }}
+          />English
+        </label>
+      </form>
+      <NotifyArea notifications={notifications} />
+      <GameStatus latestGameResult={latestGameResult} LANG={language} />
+      <GameGrid displayState={displayState} runGameLoop={runGameLoop} />
+
+      <div id="controls">
+        <button
+          id="btn-up"
+          class="control-btn"
+          type="button"
+          onClick={() => {
+            runGameLoop("w");
+          }}
+        >
+          ↑
+        </button>
+        <button
+          id="btn-down"
+          class="control-btn"
+          type="button"
+          onClick={() => {
+            runGameLoop("s");
+          }}
+        >
+          ↓
+        </button>
+        <button
+          id="btn-left"
+          class="control-btn"
+          type="button"
+          onClick={() => {
+            runGameLoop("a");
+          }}
+        >
+          ←
+        </button>
+        <button
+          id="btn-right"
+          class="control-btn"
+          type="button"
+          onClick={() => {
+            runGameLoop("d");
+          }}
+        >
+          →
+        </button>
+      </div>
     </div>
   );
 }
